cb1ddc4ecbcc29bee17775fa71d94984ea60001d
Nikita Trotner <nikita@ttnr.me>
2025-02-09 18:51:14 +0100

    chore(project): setup package with default structure (#1)

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.spec.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/types/**/*.ts',
+  ],
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      }
+    ]
+  },
+};
